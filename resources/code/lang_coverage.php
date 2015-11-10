f94b0538fc28b0cae5559d0938af14650f62a382
Diego Villanueva <dvillanuevavilar@gmail.com>
2015-11-10 11:59:20 +0100

    Fix error in

--- resources/code/lang_coverage.php
+++ resources/code/lang_coverage.php
@@ -1,7 +1,7 @@
 <?php
 //Sets Session language if exists, if not, sets spanish as default
 
-if($_SESSION["user_lang"]){
+if(isset($_SESSION["user_lang"])){
 	include_once "../i18n/".$_SESSION["user_lang"]."php";
 }else{
 	include_once "../i18n/es.php";
