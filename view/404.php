e331407430d7352bc130781d38c6810e500115b5
Andrés Vieira <anvieiravazquez@gmail.com>
2015-12-16 20:13:51 +0100

    Reworked 404 page

--- view/404.php
+++ view/404.php
@@ -1,2 +1,84 @@
-<h1>404 ERROR</h1>
-<h3>Not Found</h3>
+<?php
+include_once "../resources/code/models.php";
+include_once "../resources/code/lang_coverage.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>404</title>
+
+	<!-- Main CSS file -->
+	<link rel="stylesheet" href="../css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../css/owl.carousel.css" />
+	<link rel="stylesheet" href="../css/font-awesome.css" />
+	<link rel="stylesheet" href="../css/style.css" />
+	<link rel="stylesheet" href="../css/responsive.css" />
+	<link rel="stylesheet" href="../css/main.css" />
+	<link rel="stylesheet" href="../css/alertify.default.css" />
+	<link rel="stylesheet" href="../css/alertify.core.css" />
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="../images/icon/favicon.ico">
+
+
+
+
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+  </head>
+  <body>
+
+  	<!-- PRELOADER -->
+  	<div id="st-preloader">
+  		<div id="pre-status">
+  			<div class="preload-placeholder"></div>
+  		</div>
+  	</div>
+  	<!-- /PRELOADER -->
+
+
+  	<?php include("./header.php"); ?>
+
+
+  	<!-- PINCHOS -->
+  	<section id="page-404">
+  		<h1>404 - Not Found</h1>
+  		<img src="../images/788.gif" />
+
+  	</section>
+  	<!-- /PINCHOS -->
+
+
+  	<?php include("./footer.php"); ?>
+
+  	<!-- Scroll-up -->
+  	<div class="scroll-up">
+  		<ul><li><a href="#header"><i class="fa fa-angle-up"></i></a></li></ul>
+  	</div>
+
+
+  	<!-- JS -->
+  	<script type="text/javascript" src="../js/amcharts.js"></script> <!-- AmChart General Lib -->
+  	<script type="text/javascript" src="../js/pie.js"></script><!-- AmChart PieChart -->
+  	<script type="text/javascript" src="../js/light.js"></script><!-- AmChart PieChart Theme -->
+  	<script type="text/javascript" src="../js/jquery.min.js"></script><!-- jQuery -->
+  	<script type="text/javascript" src="../js/bootstrap.min.js"></script><!-- Bootstrap -->
+  	<script type="text/javascript" src="../js/jquery.parallax.js"></script><!-- Parallax -->
+  	<script type="text/javascript" src="../js/smoothscroll.js"></script><!-- Smooth Scroll -->
+  	<script type="text/javascript" src="../js/owl.carousel.min.js"></script><!-- Owl-Carousel -->
+  	<script type="text/javascript" src="../js/scripts.js"></script><!-- Scripts -->
+  	<script type="text/javascript" src="../js/main.js"></script><!-- PinchoWire Scripts -->
+  	<script type="text/javascript" src="../js/alertify.min.js"></script><!-- Alertify -->
+
+  </body>
+  </html>
